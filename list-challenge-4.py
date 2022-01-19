894b2c767e1a032249ac85702065c3c8ba7f34ca
Ony Malle <onydeveloper@gmail.com>
2022-01-19 20:44:48 +0800

    List Challenge 4 Solution

--- list-challenge-4.py
+++ list-challenge-4.py
@@ -1,13 +1,13 @@
-def find_product(lst):
+def find_product(param):
     res = []
-    for item in lst:
-        itemlst = [item]
-        tmpset = set(lst) - set(itemlst)
+    for i in range(len(param)):
         prod = 1
-        for itemset in tmpset:
-            prod *= itemset
+        for j in range(len(param)):
+            if j != i:
+                prod *= lst[j]
         res.append(prod)
     return res
 
-lst = [4,2,1,5,0]
+
+lst = [0, 2, 9, 0, 12, 25]
 print(find_product(lst))
