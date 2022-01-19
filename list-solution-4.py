4b92b426c26140b2279c5d5068a9532f373597d4
Ony Malle <onydeveloper@gmail.com>
2022-01-19 21:43:30 +0800

    Add description of the algorithm

--- list-solution-4.py
+++ list-solution-4.py
@@ -1,20 +1,30 @@
 # Solution 1
-# def find_product(lst):
-#     result = []
-#     left = 1  # To store product of all previous values from currentIndex
-#     for i in range(len(lst)):
-#         currentproduct = 1  # To store current product for index i
-#         # compute product of values to the right of i index of list
-#         for ele in lst[i+1:]:
-#             currentproduct = currentproduct * ele
-#         # currentproduct * product of all values to the left of i index
-#         result.append(currentproduct * left)
-#         # Updating `left`
-#         left = left * lst[i]
-#
-#     return result
+"""This solution iterates over the list and calculates
+the product of all the numbers to the right of the current element as on lines 7 and 8.
+Then it calculates the product of all the elements to the left of the current element line 10.
+It then multiplies the two products and returns the result line 14."""
+"""def find_product(lst):
+    result = []
+    left = 1  # To store product of all previous values from currentIndex
+    for i in range(len(lst)):
+        currentproduct = 1  # To store current product for index i
+        # compute product of values to the right of i index of list
+        for ele in lst[i+1:]:
+            currentproduct = currentproduct * ele
+        # currentproduct * product of all values to the left of i index
+        result.append(currentproduct * left)
+        # Updating `left`
+        left = left * lst[i]
+
+    return result"""
 
 # Solution 2
+"""The algorithm for this solution is to first create a new list
+with products of all elements to the left of each element as done on lines 4-6.
+Then multiply each element in that list to the product of all the elements
+to the right of the list by traversing it in reverse as done on lines 9-11"""
+
+
 def find_product(lst):
     # get product start from left
     left = 1
