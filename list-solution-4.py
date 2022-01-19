3fa329198f47dfd41b744efef09219ada61303e5
Ony Malle <onydeveloper@gmail.com>
2022-01-19 21:35:31 +0800

    List Solution 4

--- list-solution-4.py
+++ list-solution-4.py
@@ -1,34 +1,34 @@
 # Solution 1
-def find_product(lst):
-    result = []
-    left = 1  # To store product of all previous values from currentIndex
-    for i in range(len(lst)):
-        currentproduct = 1  # To store current product for index i
-        # compute product of values to the right of i index of list
-        for ele in lst[i+1:]:
-            currentproduct = currentproduct * ele
-        # currentproduct * product of all values to the left of i index
-        result.append(currentproduct * left)
-        # Updating `left`
-        left = left * lst[i]
-
-    return result
+# def find_product(lst):
+#     result = []
+#     left = 1  # To store product of all previous values from currentIndex
+#     for i in range(len(lst)):
+#         currentproduct = 1  # To store current product for index i
+#         # compute product of values to the right of i index of list
+#         for ele in lst[i+1:]:
+#             currentproduct = currentproduct * ele
+#         # currentproduct * product of all values to the left of i index
+#         result.append(currentproduct * left)
+#         # Updating `left`
+#         left = left * lst[i]
+#
+#     return result
 
 # Solution 2
-# def find_product(lst):
-#     # get product start from left
-#     left = 1
-#     product = []
-#     for ele in lst:
-#         product.append(left)
-#         left = left * ele
-#     # get product starting from right
-#     right = 1
-#     for i in range(len(lst)-1, -1, -1):
-#         product[i] = product[i] * right
-#         right = right * lst[i]
-#
-#     return product
+def find_product(lst):
+    # get product start from left
+    left = 1
+    product = []
+    for ele in lst:
+        product.append(left)
+        left = left * ele
+    # get product starting from right
+    right = 1
+    for i in range(len(lst)-1, -1, -1):
+        product[i] = product[i] * right
+        right = right * lst[i]
+
+    return product
 
 
 print(find_product([1, 2, 3, 4]))
